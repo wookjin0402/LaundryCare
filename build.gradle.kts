438a9a8619c6464584fb4dc6f50de12c8ec6975b
home <a99352347@gmail.com>
2026-04-03 04:37:15 +0900

    Fix: 프로젝트 초기 환경 세팅 및 빌드 에러 완전 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("com.android.application") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false // 👈 여기 숫자를 올렸습니다!
     id("com.google.gms.google-services") version "4.4.1" apply false
-    alias(libs.plugins.android.application) apply false
 }
