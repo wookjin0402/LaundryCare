7c8889c6de8d4e8f60351c7ca655f066b5c502ef
home <a99352347@gmail.com>
2026-04-11 21:40:24 +0900

    인터페이스 기초 구현 및 메인화면 변경

--- app/src/main/java/com/example/laundrycare_android/MainActivity.kt
+++ app/src/main/java/com/example/laundrycare_android/MainActivity.kt
@@ -1,39 +1,4 @@
 package com.example.laundrycare_android
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-
-        // 앱을 처음 켰을 때 기본으로 '홈' 화면을 보여줌
-        if (savedInstanceState == null) {
-            replaceFragment(HomeFragment())
-        }
-
-        // 하단 메뉴 클릭 시 동작 설정
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_home -> replaceFragment(HomeFragment())
-                R.id.nav_scan -> replaceFragment(ScanFragment())
-                R.id.nav_wardrobe -> replaceFragment(WardrobeFragment())
-                R.id.nav_settings -> replaceFragment(SettingsFragment())
-            }
-            true
-        }
-    }
-
-    // 화면을 교체해 주는 핵심 함수
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.frame_layout, fragment)
-            .commit()
-    }
+class MainActivity {
 }
